de2236a14297679c7952ffd2269645248b70f815
Collins Lagat <cklagat@gmail.com>
2023-04-02 20:09:33 +0300

    feat: refactor and complete the calculator and wrapper around the client

--- app/models/spree_avior_tax/avior_tax.rb
+++ app/models/spree_avior_tax/avior_tax.rb
@@ -1,7 +1,72 @@
 module SpreeAviorTax
   class AviorTax
-    attr_reader :order, :shipment, :client
+    attr_reader :order, :line_item, :shipment, :client
 
-    def initialize; end
+    def initialize
+      @client = SpreeAviorTax::Client.new client_params
+    end
+
+    def calculate_for_order(order, line_item)
+      @order = order
+      @line_item = line_item
+      client.calculate_tax [line_item_params]
+    end
+
+    def calculate_for_shipment(order, shipment)
+      @order = order
+      @shipment = shipment
+      client.calculate_tax [shipment_params]
+    end
+
+    private
+
+    def client_params
+      {
+        service_url: SpreeAviorTax::Config[:service_url],
+        token: SpreeAviorTax::Config[:token]
+      }
+    end
+
+    def line_item_params
+      SpreeAviorTax::API::Input::Product.new(
+        date: order.updated_at.strftime('%Y%m%d'),
+        record_number: '523355',
+        seller_id: 'S00010022',
+        seller_location_id: '1',
+        seller_state: 'FL',
+        delivery_method: 'N',
+        customer_entity_code: 'T',
+        ship_to_address: order.tax_address.address1,
+        ship_to_suite: order.tax_address.address2,
+        ship_to_city: order.tax_address.city,
+        ship_to_county: 'Miami-Dade',
+        ship_to_state: order.tax_address.state.abbr,
+        ship_to_zip_code: order.tax_address.zipcode,
+        ship_to_zip_plus: '',
+        sku: line_item.variant.sku,
+        amount_of_sale: line_item.discounted_amount
+      )
+    end
+
+    def shipment_params
+      SpreeAviorTax::API::Input::Product.new(
+        date: order.updated_at.strftime('%Y%m%d'),
+        record_number: '523355',
+        seller_id: 'S00010022',
+        seller_location_id: '1',
+        seller_state: 'FL',
+        delivery_method: 'N',
+        customer_entity_code: 'T',
+        ship_to_address: order.tax_address.address1,
+        ship_to_suite: order.tax_address.address2,
+        ship_to_city: order.tax_address.city,
+        ship_to_county: 'Miami-Dade',
+        ship_to_state: order.tax_address.state.abbr,
+        ship_to_zip_code: order.tax_address.zipcode,
+        ship_to_zip_plus: '',
+        sku: '',
+        amount_of_sale: shipment.discounted_cost
+      )
+    end
   end
 end
