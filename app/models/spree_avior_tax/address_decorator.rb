002fe50b946e9365f4707660218f74b422f35f80
Collins Lagat <cklagat@gmail.com>
2023-04-07 01:25:59 +0300

    fix: can't convert symbol to string

--- app/models/spree_avior_tax/address_decorator.rb
+++ app/models/spree_avior_tax/address_decorator.rb
@@ -1,7 +1,7 @@
 module SpreeAviorTax
   module AddressDecorator
     def self.prepended(base)
-      base::ADDRESS_FIELDS << :county unless base::ADDRESS_FIELDS.include?(:county)
+      base::ADDRESS_FIELDS << 'county' unless base::ADDRESS_FIELDS.include?('county')
       base.validates :county, presence: true
     end
   end
