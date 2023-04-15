2f66b8d0f431423828f23e26a0bbd966c34e4cef
Collins Lagat <cklagat@gmail.com>
2023-04-15 16:06:06 +0300

    feat: tax_rate decorator tests

--- spec/spree_avior_tax/tax_rate_decorator_spec.rb
+++ spec/spree_avior_tax/tax_rate_decorator_spec.rb
@@ -1,15 +1,35 @@
-describe SpreeAviorTax::OrderDecorator do
-  let(:order) { create(:avior_tax_order) }
+describe SpreeAviorTax::TaxRateDecorator do
+  context 'when using AviorTaxCalculator' do
+    let(:calculator) { build_stubbed :avior_tax_calculator }
+    let(:tax_rate) { build_stubbed :tax_rate, calculator: calculator }
 
-  describe '#avior_tax_total' do
-    it 'returns the correct avior tax total' do
-      expect(order.avior_tax_total).to eq 0.7
+    describe '#label' do
+      subject { tax_rate.send :label }
+
+      it { is_expected.to eq Spree.t(:avior_tax) }
+    end
+
+    describe '#using_avior_tax?' do
+      subject { tax_rate.send :using_avior_tax? }
+
+      it { is_expected.to be true }
     end
   end
 
-  describe '#avior_tax_included' do
-    it 'returns false' do
-      expect(order.avior_tax_included).to eq false
+  context 'when not using AviorTaxCalculator' do
+    let(:calculator) { build_stubbed :default_tax_calculator }
+    let(:tax_rate) { build_stubbed :tax_rate, calculator: calculator, show_rate_in_label: false }
+
+    describe '#label' do
+      subject { tax_rate.send :label }
+
+      it { is_expected.to eq tax_rate.name }
+    end
+
+    describe '#using_avior_tax?' do
+      subject { tax_rate.send :using_avior_tax? }
+
+      it { is_expected.to be false }
     end
   end
 end
